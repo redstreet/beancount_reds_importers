62fb7d4a1d560f003a5d1463f6a013bdda3a447b
Red S <redstreet@users.noreply.github.com>
2020-10-10 21:02:52 -0700

    add techcu bank

---
+++ beancount_reds_importers/techcubank/__init__.py
@@ -0,0 +1,16 @@
+"""Tech CU checking/savings bank account ofx importer for beancount."""
+
+import ntpath
+# import beancount_reds_importers.libimport.banking
+from .libimport import banking
+
+class Importer(libimport.banking.Importer):
+    def custom_init(self):
+        if not self.custom_init_run:
+            self.max_rounding_error = 0.04
+            self.account_number_field = 'account_id'
+            self.filename_identifier_substring = 'transactions'
+            self.filename_identifier_substring = 'Accounts'
+            self.custom_init_run = True
+
+
