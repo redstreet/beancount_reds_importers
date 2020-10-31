f2302c5117b7846be2128b8d12e9f7e24e177566
Red S <redstreet@users.noreply.github.com>
2020-10-31 02:33:27 -0700

    import fix

--- beancount_reds_importers/techcubank/__init__.py
+++ beancount_reds_importers/techcubank/__init__.py
@@ -1,10 +1,9 @@
 """Tech CU checking/savings bank account ofx importer for beancount."""
 
 import ntpath
-# import beancount_reds_importers.libimport.banking
-from .libimport import banking
+import beancount_reds_importers.libimport.banking
 
-class Importer(libimport.banking.Importer):
+class Importer(beancount_reds_importers.libimport.banking.Importer):
     def custom_init(self):
         if not self.custom_init_run:
             self.max_rounding_error = 0.04
