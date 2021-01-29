ea265289f3582de4e36d2435917afeb462833db2
Red S <redstreet@users.noreply.github.com>
2021-01-28 20:43:45 -0800

    moved DIVIDEND special case to brokerage-specific importers

--- beancount_reds_importers/vanguard/__init__.py
+++ beancount_reds_importers/vanguard/__init__.py
@@ -15,3 +15,9 @@ class Importer(beancount_reds_importers.libimport.investments.Importer,
 
     def file_name(self, file):
         return 'vanguard-all-{}'.format(ntpath.basename(file.name))
+
+    def get_target_acct(self, transaction):
+        m = transaction.memo
+        if 'DIVIDEND' in m:
+            return self.config['dividends']
+        return self.target_account_map.get(transaction.type, None)
