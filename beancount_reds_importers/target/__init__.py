8397f3e1d7471258a86bf0feb0d397066b17390d
Red S <redstreet@users.noreply.github.com>
2021-02-05 00:16:50 -0800

    add Target importer

---
+++ beancount_reds_importers/target/__init__.py
@@ -0,0 +1,13 @@
+"""Target credit card ofx importer for beancount."""
+
+from beancount_reds_importers.libreader import ofxreader
+from beancount_reds_importers.libtransactionbuilder import banking
+
+
+class Importer(banking.Importer, ofxreader.Importer):
+    def custom_init(self):
+        if not self.custom_init_run:
+            self.max_rounding_error = 0.04
+            self.account_number_field = 'account_id'
+            self.filename_identifier_substring = 'Transactions'
+            self.custom_init_run = True
