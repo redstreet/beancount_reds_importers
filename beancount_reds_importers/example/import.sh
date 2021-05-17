b7ffe9eef92ba5a89bab14113f0d78ac54b2cad9
Red S <redstreet@users.noreply.github.com>
2021-05-17 02:26:20 -0700

    add smart_importer based example

--- beancount_reds_importers/example/import.sh
+++ beancount_reds_importers/example/import.sh
@@ -5,8 +5,8 @@
 file=$1
 beancount_main=my.beancount
 
-bean-identify my.import $file
-bean-extract my.import -f $beancount_main $file
+bean-identify my-smart.import $file
+bean-extract my-smart.import -f $beancount_main $file
 
 # mkdir -pv filed
 # bean-file -o filed my.import $file
