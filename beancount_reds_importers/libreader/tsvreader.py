af80aa76a50750cb48bcd4ace565d3c4f8fa2489
Red S <redstreet@users.noreply.github.com>
2022-05-21 17:48:55 -0700

    add new importer: vanguard screenscraped

---
+++ beancount_reds_importers/libreader/tsvreader.py
@@ -0,0 +1,12 @@
+"""tsv (tab separated values) importer module for beancount to be used along with investment/banking/other
+importer modules in beancount_reds_importers."""
+
+from beancount.ingest import importer
+import petl as etl
+from beancount_reds_importers.libreader import csvreader
+
+
+class Importer(csvreader.Importer, importer.ImporterProtocol):
+    FILE_EXT = 'tsv'
+    def read_raw(self, file):
+        return etl.fromtsv(file.name)
---
+++ beancount_reds_importers/vanguard_screenscrape/__init__.py
@@ -0,0 +1,62 @@
+""" Vanguard screenscrape importer. Unsettled trades are not available in Vanguard's qfx and need to be
+screenscrapped into a tsv"""
+
+from beancount_reds_importers.libreader import tsvreader
+from beancount_reds_importers.libtransactionbuilder import investments
+import petl as etl
+
+
+class Importer(investments.Importer, tsvreader.Importer):
+    IMPORTER_NAME = 'Vanguard screensrape tsv'
+
+    def custom_init(self):
+        self.max_rounding_error = 0.04
+        self.filename_pattern_def = '.*vanguardss.*'
+        self.header_identifier = ''
+        self.get_ticker_info = self.get_ticker_info_from_id
+        self.date_format = '%m/%d/%Y'
+        self.funds_db_txt = 'funds_by_ticker'
+        self.skip_head_rows = 0
+        self.skip_tail_rows = 0
+        self.header_map = {
+            "date":        'date',
+            "settledate":   'tradeDate',
+            "symbol":      'security',
+            "description": 'memo',
+            "action":      'type',
+            "quantity":    'units',
+            "price":       'unit_price',
+            "fees":        'fees',
+            "amount":      'amount',
+            "total":       'total',
+            }
+        self.transaction_type_map = {
+            'Buy':                          'buystock',
+            'Sell':                         'sellstock',
+            }
+        self.skip_transaction_types = ['']
+
+    def prepare_raw_columns(self, rdr):
+        def extract_numbers(x):
+            replacements = {'– ': '-',
+                            '$': '',
+                            ',': '',
+                            'Free': '0',
+                            }
+            for k, v in replacements.items():
+                x = x.replace(k, v)
+            return x
+
+        header = ('date', 'settledate', 'symbol', 'description', 'quantity', 'price', 'fees', 'amount')
+        rdr = rdr.pushheader(header)
+
+        rdr = rdr.addfield('action', lambda x: x['description'].rsplit(' ', 2)[1].strip())
+
+        for field in ["date", "settledate", "symbol", "description", "quantity", "price", "fees"]:
+            rdr = rdr.convert(field, lambda x: x.strip())
+        for field in ["quantity", "amount", "price", "fees"]:
+            rdr = rdr.convert(field, extract_numbers)
+
+        rdr = rdr.addfield('total', lambda x: x['amount'])
+
+        return rdr
