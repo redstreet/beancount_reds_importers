5753ef7a7a234d26f0cfd43f59b5928335fbcf4f
Red S <redstreet@users.noreply.github.com>
2022-10-23 19:15:43 -0700

    feat (importer): add capitalone 360 ofx

---
+++ beancount_reds_importers/capitalonebank/__init__.py
@@ -0,0 +1,14 @@
+"""Capital One Bank (Checking, Savings accounts) ofx importer for beancount."""
+
+from beancount_reds_importers.libreader import ofxreader
+from beancount_reds_importers.libtransactionbuilder import banking
+
+
+class Importer(banking.Importer, ofxreader.Importer):
+    IMPORTER_NAME = 'Capital One Bank'
+
+    def custom_init(self):
+        if not self.custom_init_run:
+            self.max_rounding_error = 0.04
+            self.filename_pattern_def = '.*360Checking'
+            self.custom_init_run = True
