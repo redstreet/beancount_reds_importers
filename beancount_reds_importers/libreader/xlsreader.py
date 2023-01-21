d3b0bc7b6be5ae75dc0eb74abe4fbd3c8a840c7e
Red S <redstreet@users.noreply.github.com>
2023-01-21 02:28:11 -0800

    feat: xls reader (previous one was xlsx reader)

---
+++ beancount_reds_importers/libreader/xlsreader.py
@@ -0,0 +1,20 @@
+"""xlsx importer module for beancount to be used along with investment/banking/other importer modules in
+beancount_reds_importers."""
+
+import petl as etl
+from beancount_reds_importers.libreader import csvreader
+
+
+class Importer(csvreader.Importer):
+    FILE_EXTS = ['xls']
+
+    def initialize_reader(self, file):
+        if getattr(self, 'file', None) != file:
+            self.file = file
+            self.file_read_done = False
+            self.reader_ready = True
+
+            self.currency = self.config.get('currency', 'USD')
+
+    def read_raw(self, file):
+        return etl.fromxls(file.name, read_only=True)
