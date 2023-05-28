b888b4022503c3e648802a627ca0d36cf5248e45
Matt Brown <matt@mdbrown.dev>
2023-05-28 11:27:39 -0700

    test: Add Vanguard 401(k) test

---
+++ beancount_reds_importers/importers/vanguard/tests/vanguard_test.py
@@ -0,0 +1,31 @@
+from os import path
+from beancount.ingest import regression_pytest as regtest
+from beancount_reds_importers.importers import vanguard
+
+
+@regtest.with_importer(
+    vanguard.Importer(
+        {
+            "account_number": "444555",
+            "main_account": "Assets:Vanguard:401k:{ticker}",
+            "cash_account": "Assets:Vanguard:401k:Cash",
+            "dividends": "Income:Dividends:Vanguard:401k:{ticker}",
+            "interest": "Income:Interest:Vanguard:401k:{ticker}",
+            "cg": "Income:CapitalGains:401k:{ticker}",
+            "capgainsd_lt": "Income:CapitalGains:Long:Vanguard:401k:{ticker}",
+            "capgainsd_st": "Income:CapitalGains:Short:Vanguard:401k:{ticker}",
+            "fees": "Expenses:Fees:Vanguard:401k",
+            "rounding_error": "Equity:Rounding-Errors:Imports",
+            "fund_info": {
+                "fund_data": [
+                    # https://investor.vanguard.com/mutual-funds/profile/pe/overview/7743
+                    ("V7743", "VGI007743", "Vanguard Target Retirement 2050 Trust"),
+                ],
+                "money_market": ["VMFXX"],
+            },
+        }
+    )
+)
+@regtest.with_testdir(path.dirname(__file__))
+class TestVanguard(regtest.ImporterTestBase):
+    pass
