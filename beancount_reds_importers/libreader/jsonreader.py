7400c60c307adaf222194148e6571a310f3c77a0
Red S <redstreet@users.noreply.github.com>
2024-02-10 16:58:05 -0800

    ci: fix ruff

--- beancount_reds_importers/libreader/jsonreader.py
+++ beancount_reds_importers/libreader/jsonreader.py
@@ -1,5 +1,10 @@
+
 """JSON importer module for beancount to be used along with investment/banking/other importer modules in
 beancount_reds_importers.
+
+------------------------------
+This is WIP and incomplete.
+------------------------------
 
 JSON schemas vary widely. This one is based on Charles Schwab's json format. In the future, the
 goal is to make this reader automatically "understand" the schema of any json given to it.
@@ -7,14 +12,14 @@ goal is to make this reader automatically "understand" the schema of any json gi
 Until that happens, perhaps this file should be renamed to schwabjsonreader.py.
 """
 
-import datetime
-import ofxparse
-from collections import namedtuple
+# import datetime
+# import ofxparse
+# from collections import namedtuple
 from beancount.ingest import importer
 from beancount_reds_importers.libreader import reader
 from bs4.builder import XMLParsedAsHTMLWarning
 import json
-import re
+# import re
 import warnings
 warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
@@ -43,44 +48,44 @@ class Importer(reader.Reader, importer.ImporterProtocol):
         with open(file.name) as fh:
             self.rdr = json.load(fh)
 
-            transactions = []
-            for transaction in self.rdr['BrokerageTransactions']:
-                raw_ot = Transaction(
-                    date       = transaction['Date'],
-                    type       = transaction['Action'],
-                    security   = transaction['Symbol'],
-                    memo       = transaction['Description'],
-                    unit_price = transaction['Price'],
-                    units      = transaction['Quantity'],
-                    fees       = transaction['Fees & Comm'],
-                    total      = transaction['Amount']
-                )
+            # transactions = []
+            # for transaction in self.rdr['BrokerageTransactions']:
+            #     raw_ot = Transaction(
+            #         date       = transaction['Date'],
+            #         type       = transaction['Action'],
+            #         security   = transaction['Symbol'],
+            #         memo       = transaction['Description'],
+            #         unit_price = transaction['Price'],
+            #         units      = transaction['Quantity'],
+            #         fees       = transaction['Fees & Comm'],
+            #         total      = transaction['Amount']
+            #     )
 
 
 
-    def get_transactions(self):
-        Transaction = namedtuple('Transaction', ['date', 'type', 'security', 'memo', 'unit_price',
-                                                 'units', 'fees', 'total'])
-        for transaction in self.rdr['BrokerageTransactions']:
-            raw_ot = Transaction(
-                date       = transaction['Date'],
-                type       = transaction['Action'],
-                security   = transaction['Symbol'],
-                memo       = transaction['Description'],
-                unit_price = transaction['Price'],
-                units      = transaction['Quantity'],
-                fees       = transaction['Fees & Comm'],
-                total      = transaction['Amount']
-            )
-            ot = self.fixup(ot)
-            import pdb; pdb.set_trace()
-            yield ot
+    # def get_transactions(self):
+    #     Transaction = namedtuple('Transaction', ['date', 'type', 'security', 'memo', 'unit_price',
+    #                                              'units', 'fees', 'total'])
+    #     for transaction in self.rdr['BrokerageTransactions']:
+    #         raw_ot = Transaction(
+    #             date       = transaction['Date'],
+    #             type       = transaction['Action'],
+    #             security   = transaction['Symbol'],
+    #             memo       = transaction['Description'],
+    #             unit_price = transaction['Price'],
+    #             units      = transaction['Quantity'],
+    #             fees       = transaction['Fees & Comm'],
+    #             total      = transaction['Amount']
+    #         )
+    #         ot = self.fixup(ot)
+    #         import pdb; pdb.set_trace()
+    #         yield ot
 
     def fixup(self, ot):
         ot.date = self.convert_date(ot.date)
 
-    def convert_date(d):
-        return datetime.datetime.strptime(d, self.date_format)
+    # def convert_date(d):
+    #     return datetime.datetime.strptime(d, self.date_format)
 
     def get_balance_assertion_date(self):
         return None
