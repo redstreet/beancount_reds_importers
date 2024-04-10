cc6feaf7abc1331a337b2729c4b5c024e0c4c242
Red S <redstreet@users.noreply.github.com>
2024-04-10 16:49:00 -0700

    fix: schwab_csv_creditline balance sign

--- beancount_reds_importers/importers/schwab/schwab_csv_creditline.py
+++ beancount_reds_importers/importers/schwab/schwab_csv_creditline.py
@@ -1,6 +1,7 @@
 """Schwab Credit Line (eg: Pledged Asset Line) .csv importer."""
 
 from beancount_reds_importers.importers.schwab import schwab_csv_checking
+from beancount_reds_importers.libtransactionbuilder import banking
 
 
 class Importer(schwab_csv_checking.Importer):
@@ -12,3 +13,9 @@ class Importer(schwab_csv_checking.Importer):
         self.column_labels_line = (
             '"Date","Type","CheckNumber","Description","Withdrawal","Deposit","RunningBalance"'
         )
+
+    def get_balance_statement(self, file=None):
+        """Return the balance on the first and last dates"""
+
+        for i in super().get_balance_statement(file):
+            yield banking.Balance(i.date, -1 * i.amount, i.currency)
