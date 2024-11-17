redstreet/beancount_reds_importers

commit 07d972d95e535b0705d52e7ce36a25c40d4babaf

feat: turn on emit_filing_account by default
Else, we are dependent on the order of postings determined by Python

beancount_reds_importers/libtransactionbuilder/transactionbuilder.py
@@ -54,7 +54,7 @@ class TransactionBuilder:
         # This 'filing_account' is read by a patch to bean-extract so it can output transactions to
         # a file that corresponds with filing_account, when the one-file-per-account feature is
         # used.
-        if self.config.get("emit_filing_account_metadata"):
+        if not self.config.get("emit_filing_account_metadata", False):
             acct = self.config.get("filing_account", self.config.get("main_account", None))
             return {"filing_account": acct}
         return {}

commit 9daa22b0827c8deaef551a598cd047647b5bd61c

fix: failing unit tests because of emit_filing_account change
- emit_filing_account was turned on by default in 07d972d, which broke
  unit tests

beancount_reds_importers/libtransactionbuilder/transactionbuilder.py
@@ -54,7 +54,7 @@ class TransactionBuilder:
         # This 'filing_account' is read by a patch to bean-extract so it can output transactions to
         # a file that corresponds with filing_account, when the one-file-per-account feature is
         # used.
-        if not self.config.get("emit_filing_account_metadata", False):
+        if self.config.get("emit_filing_account_metadata", True) is not False:
             acct = self.config.get("filing_account", self.config.get("main_account", None))
             return {"filing_account": acct}
         return {}

beancount_reds_importers/importers/ally/tests/ally_test.py
@@ -10,6 +10,7 @@ from beancount_reds_importers.importers import ally
         {
             "account_number": "23456",
             "main_account": "Assets:Banks:Checking",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/importers/capitalonebank/tests/capitalone_test.py
@@ -10,6 +10,7 @@ from beancount_reds_importers.importers import capitalonebank
         {
             "account_number": "9876",
             "main_account": "Assets:Banks:CapitalOne",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/importers/dcu/tests/dcu_csv_test.py
@@ -12,6 +12,7 @@ from beancount_reds_importers.importers import dcu
         {
             "main_account": "Assets:Banks:DCU:Checking",
             "currency": "USD",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/importers/etrade/tests/etrade_qfx_brokerage_test.py
@@ -41,6 +41,7 @@ def build_config():
         "rounding_error": "Equity:Rounding-Errors:Imports",
         "fund_info": fund_info,
         "currency": currency,
+        "emit_filing_account_metadata": False,
     }
     return config
 

beancount_reds_importers/importers/schwab/tests/schwab_csv_brokerage/schwab_csv_brokerage_test.py
@@ -42,6 +42,7 @@ def build_config():
         "rounding_error": "Equity:Rounding-Errors:Imports",
         "fund_info": fund_info,
         "currency": currency,
+        "emit_filing_account_metadata": False,
     }
     return config
 

beancount_reds_importers/importers/schwab/tests/schwab_csv_checking/schwab_csv_checking_test.py
@@ -13,6 +13,7 @@ from beancount_reds_importers.importers.schwab import schwab_csv_checking
             "account_number": "1234",
             "main_account": "Assets:Banks:Schwab",
             "currency": "USD",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/importers/unitedoverseas/tests/uobbank_test.py
@@ -12,6 +12,7 @@ from beancount_reds_importers.importers.unitedoverseas import uobbank
             "account_number": "1234567890",
             "currency": "SGD",
             "rounding_error": "Equity:Rounding-Errors:Imports",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/importers/vanguard/tests/vanguard_test.py
@@ -26,6 +26,7 @@ from beancount_reds_importers.importers import vanguard
                 ],
                 "money_market": ["VMFXX"],
             },
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/libreader/tests/balance_assertion_date/last_transaction/last_transaction_date_test.py
@@ -11,6 +11,7 @@ from beancount_reds_importers.importers import ally
             "account_number": "23456",
             "main_account": "Assets:Banks:Checking",
             "balance_assertion_date_type": "last_transaction",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/libreader/tests/balance_assertion_date/ofx_date/ofx_date_test.py
@@ -11,6 +11,7 @@ from beancount_reds_importers.importers import ally
             "account_number": "23456",
             "main_account": "Assets:Banks:Checking",
             "balance_assertion_date_type": "ofx_date",
+            "emit_filing_account_metadata": False,
         }
     )
 )

beancount_reds_importers/libreader/tests/balance_assertion_date/smart/smart_date_test.py
@@ -11,6 +11,7 @@ from beancount_reds_importers.importers import ally
         {
             "account_number": "23456",
             "main_account": "Assets:Banks:Checking",
+            "emit_filing_account_metadata": False,
         }
     )
 )