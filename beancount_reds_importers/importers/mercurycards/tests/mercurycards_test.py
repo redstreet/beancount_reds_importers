d176cc7674d7d4daaaf5376c9a6bbe6f58bb866c
gary-roach <79265780+gary-roach@users.noreply.github.com>
2024-11-21 23:52:18 -0500

    feat: improvements to pdfreader
    Refactored pdfreader to be more extensible and added a mercurycards importer which uses the pdfreader to read credit card transactions.

---
+++ beancount_reds_importers/importers/mercurycards/tests/mercurycards_test.py
@@ -0,0 +1,21 @@
+from os import path
+
+from beancount.ingest import regression_pytest as regtest
+
+from beancount_reds_importers.importers import mercurycards
+
+
+@regtest.with_importer(
+    mercurycards.Importer(
+        {
+            "main_account": "Liabilities:Credit-Cards:Mercury",
+            "emit_filing_account_metadata": False,
+            "filename_pattern": "mercury_statement_20241105.pdf",
+            "skip_transaction_types": {},
+            "currency": "USD",
+        }
+    )
+)
+@regtest.with_testdir(path.dirname(__file__))
+class TestMercuryCards(regtest.ImporterTestBase):
+    pass
