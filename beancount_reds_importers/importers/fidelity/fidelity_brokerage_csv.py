e9e0acc0e08924318d54f02aef76cae8eb47ec22
rnicholus <rnicholus@users.noreply.github.com>
2025-01-05 18:41:35 -0600

    cap gains type update

--- beancount_reds_importers/importers/fidelity/fidelity_brokerage_csv.py
+++ beancount_reds_importers/importers/fidelity/fidelity_brokerage_csv.py
@@ -35,8 +35,8 @@ class Importer(investments.Importer, csvreader.Importer):
             "YOU SOLD": "sellstock",
             "INTEREST EARNED": "income",
             "REINVESTMENT FIDELITY": "reinvest",
-            "SHORT-TERM CAP GAIN": "capgainsd_st",
-            "LONG-TERM CAP GAIN": "capgainsd_lt",
+            "SHORT-TERM CAP": "capgainsd_st",
+            "LONG-TERM CAP": "capgainsd_lt",
         }
         self.skip_transaction_types = []
         # fmt: on
