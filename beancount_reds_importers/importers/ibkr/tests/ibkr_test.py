adc99ed196dba9b4ff15320b6c271dec492efe87
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2025-05-22 09:38:45 +0200

    style: reformat

--- beancount_reds_importers/importers/ibkr/tests/ibkr_test.py
+++ beancount_reds_importers/importers/ibkr/tests/ibkr_test.py
@@ -1,20 +1,24 @@
-'''
+"""
 Tests for the IBKR importer.
-'''
+"""
+
 from os import path
 
 from beancount_reds_importers.importers import ibkr
 from beancount_reds_importers.util import regression_pytest as regtest
 
+
 @regtest.with_importer(
-    ibkr.Importer({
-
-    })
+    ibkr.Importer(
+        {
+            "account_number": "U1110000",
+        }
+    )
 )
-
 @regtest.with_testdir(path.dirname(__file__))
 class TestIbkr(regtest.ImporterTestBase):
-    '''
+    """
     Tests for the IBKR importer.
-    '''
-    pass
+    """
+
+    assert False
