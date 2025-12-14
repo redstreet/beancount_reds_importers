3ad9043ed49aa11f73f6f02f789ddaf78c36262e
ware <wareadams@washdcmail.com>
2025-12-14 14:19:49 -0500

    fix: upper case for consistency

--- beancount_reds_importers/importers/fidelity/fidelity_all_accounts_csv.py
+++ beancount_reds_importers/importers/fidelity/fidelity_all_accounts_csv.py
@@ -78,10 +78,10 @@ class Importer(csvreader.Importer, investments.Importer):
             "PART CONTRIB": "dep",
             "ROLLOVER SHARES": "buystock",  # rollover from closed account...almost certainly needs to be edited manually
             "CONVERSION as": "buymf",  # conversion of mutual fund class...almost certainly needs to be edited manually
-            "Exchange Out": "sellmf",
-            "Exchange In": "buymf",
-            "Change on": "capgainsd_lt",
-            "Withdrawal": "sellmf",
+            "EXCHANGE OUT": "sellmf",
+            "EXCHANGE IN": "buymf",
+            "CHANGE ON": "capgainsd_lt",
+            "WITHDRAWAL": "sellmf",
         }
         # fmt: on
 
